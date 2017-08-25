32ce2109801f9f398e2b96ed73f854c513c5d59e
Diego Abanto <saint_dieguito@hotmail.com>
2017-08-25 16:42:07 -0500

    PANTALLA DE DETALLE Y SUS FUNCIONES

--- src/pages/detalle/detalle.component.ts
+++ src/pages/detalle/detalle.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 //Import NavController , NavParams
 import { NavController, NavParams } from 'ionic-angular';
+//Import Lista, ListaItem
+import { Lista, ListaItem } from '../../app/clases/index';
+//Import ListaDeseosService
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
 
 @Component({
   selector: 'app-detalle',
@@ -9,9 +13,11 @@ import { NavController, NavParams } from 'ionic-angular';
 export class DetalleComponent implements OnInit {
 
   idx:number;
-  lista:any;
+  lista:Lista;
 
-  constructor( public navCtrl:NavController, public navParams:NavParams ) {
+  constructor( public navCtrl:NavController,
+    public navParams:NavParams,
+    public _listaDeseos:ListaDeseosService) {
 
     //console.log(this.navParams);
     this.idx =  this.navParams.get("idx");
@@ -20,4 +26,14 @@ export class DetalleComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  actualizar( item:ListaItem ){
+    item.completado = !item.completado;
+    this._listaDeseos.actualizarData();
+  }
+
+  borrarItem(){
+    console.log("Borrar Item");
+  }
+
 }
